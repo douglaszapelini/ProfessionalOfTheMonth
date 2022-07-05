14ce8f9c3e130cbbadb7a00e93a3ec8c56c41d6a
Douglas de Souza Zapelini <douglasszapelini@gmail.com>
2022-07-05 19:51:23 -0300

    Added Drag 'n Drop into Mural page

--- tcc-frontend/src/components/MuralItem/MuralItemCard.tsx
+++ tcc-frontend/src/components/MuralItem/MuralItemCard.tsx
@@ -1,51 +1,87 @@
 import { Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
 import { Component } from "react";
 import { MuralItemModel } from "../../Model/MuralItemModel";
+import { DragDropContext, Draggable, Droppable, DropResult } from "react-beautiful-dnd"
 
 interface MuralItemCardProps {
     listMuralItem: Array<MuralItemModel>
 }
  
 interface MuralItemCardState {
-    
+    listItems: Array<MuralItemModel>
 }
-
 class MuralItemCard extends Component<MuralItemCardProps, MuralItemCardState> {
     constructor(props: MuralItemCardProps) {
         super(props);
+        this.state = {listItems: props.listMuralItem};
     }
 
+    setListItems(newList: Array<MuralItemModel>){
+      
+      this.setState({listItems: newList})
+    }
+
+    onDragEnd = (result: DropResult) => {
+      const { source, destination } = result
+      if (!destination) return
+  
+      const items = Array.from(this.state.listItems)
+      const [ newOrder ] = items.splice(source.index, 1)
+      items.splice(destination.index, 0, newOrder)
+      this.setListItems(items)
+    }
 
     render() { 
-        return (
-            <Grid container justifyContent='center'>
-                {this.props.listMuralItem.map( muralItem => {
-                    return(
-                        <Card sx={{ width:'100%', marginTop: '1rem',backgroundColor: '#808080', border: '1px solid white'}}>
-                            {!!muralItem.srcImage &&
+      const {listItems} = this.state;
+
+      return (
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          <Droppable droppableId="items">
+            {(droppableProvided) => (
+              <div className="items" {...droppableProvided.droppableProps} ref={droppableProvided.innerRef}>
+                {listItems.map(({ id, title, srcImage, description, sequence }, index) => {
+                  return (
+                    <Draggable key={id} draggableId={sequence.toString()} index={index}>
+                      {(provided) => (
+                        <Grid container justifyContent='center'>
+                          <Card 
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              sx={{ width:'90%', 
+                              marginTop: '1rem',
+                              backgroundColor: '#808080', 
+                              border: '1px solid white'}}>
+                              {!!srcImage &&
                                 <CardMedia
-                                    component="img"
-                                    image={muralItem.srcImage}
+                                  component="img"
+                                  image={srcImage}
                                 />
-                            }
-                            <CardContent>
+                              }
+                              <CardContent>
                                 {/* CARD TITLE*/}
-                                <Grid sx={{backgroundColor: '#373737'}}>
-                                    <Typography variant="h6">
-                                        {muralItem.title}
-                                    </Typography>
+                                <Grid sx={{backgroundColor: '#373737', borderRadius:'4px'}}>
+                                  <Typography variant="h6">
+                                    {title}
+                                  </Typography>
                                 </Grid>
                                 {/* CARD DESCRIPTION*/}
                                 <Typography variant="body2" color="text.secondary">
-                                    {muralItem.title}
+                                  {description}
                                 </Typography>
-                            </CardContent>
-
-                        </Card>
-                    )
+                              </CardContent>
+                          </Card>
+                        </Grid>
+                      )}
+                    </Draggable>
+                  )
                 })}
-            </Grid>
-        );
+                {droppableProvided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      );
     }
 }
  
